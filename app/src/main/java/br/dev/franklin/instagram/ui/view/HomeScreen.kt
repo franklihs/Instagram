2e29fff7ce5f83578bc5a8d0ab675c9aefd7c647
franklihs <franklihs@hotmail.com>
2024-08-08 13:03:13 +0200

    add first story do `HomeScreen` using `StoryItem`

--- app/src/main/java/br/dev/franklin/instagram/ui/view/HomeScreen.kt
+++ app/src/main/java/br/dev/franklin/instagram/ui/view/HomeScreen.kt
@@ -7,12 +7,16 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import br.dev.franklin.instagram.data.Story
 
 @Composable
 fun HomeScreen() {
 
-  Column(modifier = Modifier.fillMaxSize().background(MaterialTheme.colorScheme.background)) {
+  Column(modifier = Modifier
+    .fillMaxSize()
+    .background(MaterialTheme.colorScheme.background)) {
     InstagramToolBar()
+    StoryItem(story = Story(userNickName = "Jane Doe", userAvatar = "https://wow.zamimg.com/uploads/screenshots/normal/177061-p%C3%ADcaro-human-rogue-by-glenn-rane.jpg"))
   }
 }
 
