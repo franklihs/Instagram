cbe39e385757fcd24c2430cda43662b1fdcf4c2d
franklihs <franklihs@hotmail.com>
2024-09-06 14:59:04 +0200

    change button to go back in `MessagesScreen.kt`

--- app/src/main/java/br/dev/franklin/instagram/ui/view/MessagesScreen.kt
+++ app/src/main/java/br/dev/franklin/instagram/ui/view/MessagesScreen.kt
@@ -1,6 +1,8 @@
 package br.dev.franklin.instagram.ui.view
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -9,21 +11,26 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import br.dev.franklin.instagram.R
 import br.dev.franklin.instagram.ui.theme.InstagramTheme
 import br.dev.franklin.instagram.ui.theme.spacingLarge
 
 @Composable
 fun MessagesScreen(navController: NavController, userName: String) {
+
+  val backIcon = R.drawable.arrow_back
+
   LazyColumn(
     modifier = Modifier
       .background(MaterialTheme.colorScheme.background)
@@ -39,6 +46,14 @@ fun MessagesScreen(navController: NavController, userName: String) {
           .height(42.dp),
         verticalAlignment = Alignment.CenterVertically
       ) {
+//        Button(onClick = { navController.navigate("homeScreen/$userName") }) {
+        Image(
+          painter = painterResource(id = backIcon),
+          contentDescription = "Button to go to last screen",
+          modifier = Modifier.clickable { navController.navigate("homeScreen/$userName") }.padding(end = 12.dp),
+          colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onBackground),
+        )
+//        }
 
         Text(
           text = userName,
@@ -51,9 +66,7 @@ fun MessagesScreen(navController: NavController, userName: String) {
       Column(modifier = Modifier.fillParentMaxHeight(), verticalArrangement = Arrangement.Center)
       {
         Text(text = "You are in the Messages Screen")
-        Button(onClick = { navController.navigate("homeScreen/$userName") }) {
-          Text(text = "Back to Home Screen")
-        }
+        Text(text = "in construction...")
       }
     }
 
